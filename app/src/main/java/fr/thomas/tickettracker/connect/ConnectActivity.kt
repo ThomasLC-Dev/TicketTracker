9c0c69e1b82413a3192316c408daf93602991c54
ThomasLC-Dev <thomas.le-corvec@outlook.com>
2020-09-06 00:36:38 +0200

    Add ConnectActivity design

--- app/src/main/java/fr/thomas/tickettracker/connect/ConnectActivity.kt
+++ app/src/main/java/fr/thomas/tickettracker/connect/ConnectActivity.kt
@@ -1,11 +1,12 @@
-package fr.thomas.tickettracker
+package fr.thomas.tickettracker.connect
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import fr.thomas.tickettracker.R
 
-class MainActivity : AppCompatActivity() {
+class ConnectActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_connect)
     }
 }
